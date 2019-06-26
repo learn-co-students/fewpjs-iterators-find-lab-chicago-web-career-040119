29ad836b9608f42361942a06daea1f41dd58bd94
NMoore42 <nicholas.moore5@icloud.com>
2019-06-26 18:32:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(array) {
+  var win = array.find(function(obj) {
+    return obj.result === "W"
+  })
+  return win ? win.year : win
 }
